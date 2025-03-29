818e0ae03ff93b171a20efc03fe1a0ba153db16b
Azdim Zul Fahmi <azdim.fahmi@bytedance.com>
2025-03-29 14:20:06 +0700

    fixing android build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,16 @@
+#[tauri::command]
+fn is_mobile_platform() -> bool {
+    let platform = tauri_plugin_os::platform();
+    platform == "ios" || platform == "android"
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![is_mobile_platform])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

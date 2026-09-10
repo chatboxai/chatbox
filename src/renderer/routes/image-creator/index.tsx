cab6d6c1e4b921d2499a5bb28bdbf67045b8c07a
themez <themezeng@gmail.com>
2026-09-10 11:25:01 +0800

    fix(image): recover generated image loading failures (#1257)
    Show thumbnails while full-size images load, bound image requests to 30 seconds, and expose a reload action that preserves completed generation results. Add regression coverage and localized loading states.
    
    (cherry picked from commit 24bae838355ce975b0fd62929d8baa341e6b3292)
    
    (cherry picked from pro commit 98ed8421c9c4e30573277b694484be45efdd4480)

--- src/renderer/routes/image-creator/index.tsx
+++ src/renderer/routes/image-creator/index.tsx
@@ -574,6 +574,7 @@ function ImageCreatorPage() {
                     <Flex justify="center" w="100%">
                       <GeneratedImagesGallery
                         images={currentRecord.generatedImages}
+                        thumbnails={currentRecord.generatedImageThumbnails}
                         onUseAsReference={(urlOrKey) => handleUseAsReference(urlOrKey, currentRecord.id)}
                         onReport={() => void handleReportGeneratedImage(currentRecord)}
                       />
